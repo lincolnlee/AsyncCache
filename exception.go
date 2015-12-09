b5bd4701e2c83985f0ce7cb463a4f5b360381c7a
lincolnlee <nileel@163.com>
2015-12-09 11:07:23 +0800

    try catch

--- exception.go
+++ exception.go
@@ -5,11 +5,25 @@ import (
 	//"log"
 )
 
-type exception struct{}
+type exception struct {
+	exinfo interface{}
+}
 
-func (*exception) CatchException() {
+func (this *exception) catchException() {
 	if ex := recover(); ex != nil {
 		//log.Panic(ex)
-		fmt.Println("Exception:", ex)
+		//fmt.Println("Exception:", ex)
+		this.exinfo = ex
+	}
+}
+
+func (this *exception) Try(f func()) {
+	defer this.catchException()
+	f()
+}
+
+func (this *exception) Catch(f func(interface{})) {
+	if this.exinfo != nil {
+		f(this.exinfo)
 	}
 }
