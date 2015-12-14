5dc5f92a16a9f8ffa4b8a94074845755e47efb4b
lincolnlee <nileel@163.com>
2015-12-14 20:33:18 +0800

    update

--- async_cache.go
+++ async_cache.go
@@ -12,24 +12,32 @@ type cacheHandler struct {
 	errAmount    int32
 }
 
-func (this *cacheHandler) AsyncGetAndUpdateData(f func() interface{}, key string) interface{} {
+func (this *cacheHandler) AsyncGetAndUpdateData(f func() interface{}, key string, pEnabledCache bool) interface{} {
 	var cacheValue interface{} = nil
-	if v, err := InstanceContainer.redisClient.GetBytesSlice(key); v != nil && err == nil {
-		iSlice := InstanceContainer.serializer.DeserializeToSlice(v)
-		if len(iSlice) == 2 {
-			cacheValue = iSlice[1]
-			if time.Now().Sub(to.Time(iSlice[0])).Minutes() > 5 {
-				go this.asyncDealCacheTask(f, key, cacheValue, true)
+
+	if this.enabledCache && pEnabledCache {
+		if v, err := InstanceContainer.redisClient.GetBytesSlice(key); v != nil && err == nil {
+			iSlice := InstanceContainer.serializer.DeserializeToSlice(v)
+			if len(iSlice) == 2 {
+				cacheValue = iSlice[1]
+				if time.Now().Sub(to.Time(iSlice[0])).Minutes() > 5 {
+					go this.asyncDealCacheTask(f, key, cacheValue, true)
+				}
+
+				return cacheValue
 			}
-
-			return cacheValue
+		} else {
+			InstanceContainer.Loghelper.Error(err)
+			this.healthDetect()
 		}
-	} else {
-		InstanceContainer.Loghelper.Error(err)
-
 	}
+
 	cacheValue = f()
-	go this.asyncDealCacheTask(f, key, cacheValue, false)
+
+	if this.enabledCache && pEnabledCache {
+		go this.asyncDealCacheTask(f, key, cacheValue, false)
+	}
+
 	return cacheValue
 }
 
@@ -66,10 +74,36 @@ func (this *cacheHandler) healthDetect() {
 	if this.errAmount > 100 && this.enabledCache {
 		this.mutex.Lock()
 		if this.errAmount > 100 && this.enabledCache {
+
+			this.enabledCache = false
+			this.errAmount = 0
+			this.mutex.Unlock()
+
 			go func() {
-				this.enabledCache = true
+				hasErr := true
+				retryFrequency := 5 //重试次数
+				semaphore := make(chan int)
+
+				for hasErr {
+					semaphore = make(chan int)
+					go func() {
+						InstanceContainer.redisClient.SetString("test_key", "ok")
+						InstanceContainer.redisClient.GetString("test_key")
+						semaphore <- 1
+					}()
+
+					select {
+					case <-semaphore:
+						if retryFrequency--; retryFrequency <= 0 {
+							hasErr = false
+						}
+					case <-time.After(1 * time.Second):
+						retryFrequency = 5
+					}
+
+					time.Sleep(1 * time.Second)
+				}
 			}()
 		}
 	}
-
 }
